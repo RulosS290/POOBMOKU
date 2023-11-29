bdd7193191a758c38af26040463d1a004ff73b1b
unknown <Rulos29@outlook.es>
2023-11-29 10:21:34 -0500

    Se agrego cosas a Player

--- src/domain/Player.java
+++ src/domain/Player.java
@@ -1,15 +1,36 @@
 package domain;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class Player {
     private int puntuacion;
     private String colorName; // Cambiado a colorName para evitar conflicto con java.awt.Color
     private String nombre;
+    private String modoJuego;
+    private ArrayList<Fichas> fichas;
     public Player(String name, String color){
         nombre = name;
         colorName = color;
         puntuacion = 0;
+        fichasJugador();
+    }
+
+    public Player(String name, String color, String mode){
+        nombre = name;
+        colorName = color;
+        puntuacion = 0;
+        modoJuego = mode;
+        fichasJugador();
+    }
+
+    public void fichasJugador(){
+        int i;
+        if(modoJuego == "Normal"){
+            for(i = 0; i <= 16; i++){
+
+            }
+        }
     }
     public Color getColor(){
         switch (colorName) {
