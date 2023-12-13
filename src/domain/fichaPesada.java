6f91c1882bac6a3bfa03a43075fa42bb3930f438
Diegoc04 <diego.fercho.castellanos@gmail.com>
2023-12-12 23:21:47 -0500

    q

--- src/domain/fichaPesada.java
+++ src/domain/fichaPesada.java
@@ -1,14 +1,9 @@
 package domain;
 
 public class fichaPesada extends Fichas {
-<<<<<<< HEAD
-    private int peso = 2;
-    private String tipo;
-=======
     private final int peso = 2;
-
     private final String tipo = "Pesada";
->>>>>>> rulos
+
 
     public fichaPesada(Jugador jugador, String color) {
 
