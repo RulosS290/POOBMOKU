0770702e0e7036ef97ce8c4c2629ad97a1643f43
Diegoc04 <diego.fercho.castellanos@gmail.com>
2023-12-13 21:12:01 -0500

    hola, tu nariz contra mis bolah

--- src/domain/GomokuJuego.java
+++ src/domain/GomokuJuego.java
@@ -219,8 +219,6 @@ public class GomokuJuego implements Serializable {
         }
     }
 
-
-
     public void realizarJugadaMaquina() {
         if (jugadorActual instanceof JugadorMaquina) {
             ((JugadorMaquina) jugadorActual).realizarJugada(this);
